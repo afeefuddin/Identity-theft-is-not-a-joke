423d176b5e81494dbd96c28a2b7afba199991b7f
afeefuddin <afeefud2004din@gmail.com>
2025-07-19 18:53:26 +0530

    Main logic mostly done

--- src/controllers/contact.controller.ts
+++ src/controllers/contact.controller.ts
@@ -22,11 +22,13 @@ const contactInclude = {
       id: true,
       email: true,
       phoneNumber: true,
+      createdAt: true,
       linkingContacts: {
         select: {
           id: true,
           email: true,
           phoneNumber: true,
+          createdAt: true,
         },
       },
     },
@@ -36,6 +38,7 @@ const contactInclude = {
       id: true,
       email: true,
       phoneNumber: true,
+      createdAt: true,
     },
   },
 } as const;
@@ -66,13 +69,37 @@ function prepareOutput(data: ContactWithRelations) {
   };
 }
 
+async function createContactAndReturnData({
+  email,
+  phoneNumber,
+  linkedId,
+  linkPrecedence,
+}: {
+  email?: string;
+  phoneNumber?: string;
+  linkPrecedence?: "primary" | "secondary";
+  linkedId?: number;
+}) {
+  const data: { email?: string; phoneNumber?: string; linkedId?: number } = {};
+  if (email) data.email = email;
+  if (phoneNumber) data.phoneNumber = phoneNumber;
+  if (linkedId) data.linkedId = linkedId;
+
+  const newContact = await prisma.contact.create({
+    data: { linkPrecedence: linkPrecedence ?? "primary", ...data },
+    include: contactInclude,
+  });
+
+  return prepareOutput(newContact);
+}
+
 export async function GET(req: Request, res: Response, next: NextFunction) {
   try {
-    const parsed = contactGetBodySchema.parse(req.body);
+    const { email, phoneNumber } = contactGetBodySchema.parse(req.body);
 
     const filter: Prisma.ContactWhereInput = {};
-    if (parsed.email) filter.email = parsed.email;
-    if (parsed.phoneNumber) filter.phoneNumber = parsed.phoneNumber;
+    if (email) filter.email = email;
+    if (phoneNumber) filter.phoneNumber = phoneNumber;
 
     const exactMatch = await prisma.contact.findFirst({
       where: filter,
@@ -84,25 +111,139 @@ export async function GET(req: Request, res: Response, next: NextFunction) {
       return res.status(200).json(prepareOutput(exactMatch));
     }
 
-    if (!parsed.email || !parsed.phoneNumber) {
+    if (!email || !phoneNumber) {
       // This means we should create and return
       // This is a unique one
 
-      const data: { email?: string; phoneNumber?: string } = {};
-      if (parsed.email) data.email = parsed.email;
-      if (parsed.phoneNumber) data.phoneNumber = parsed.phoneNumber;
-
-      const newContact = await prisma.contact.create({
-        data: { linkPrecedence: "primary", ...data },
-        include: contactInclude,
+      const newContact = await createContactAndReturnData({
+        email,
+        phoneNumber,
       });
 
-      return res.status(200).json(prepareOutput(newContact));
+      return res.status(200).json(newContact);
     }
 
-    // 
+    const emailMatch = await prisma.contact.findFirst({
+      where: {
+        email,
+      },
+      include: contactInclude,
+    });
 
+    const phoneMatch = await prisma.contact.findFirst({
+      where: {
+        phoneNumber,
+      },
+      include: contactInclude,
+    });
 
+    if (!emailMatch && !phoneMatch) {
+      const newContact = await createContactAndReturnData({
+        email,
+        phoneNumber,
+      });
+
+      return res.status(200).json(newContact);
+    } else if (!phoneMatch && emailMatch) {
+      // There is email match who's primary will be the primary of new contact
+      const emailMatchPrimary =
+        emailMatch.linkPrecedence === "primary"
+          ? emailMatch
+          : emailMatch.linkedContact;
+
+      if (!emailMatchPrimary) {
+        throw new Error("Sever Error");
+      }
+
+      const newContact = await createContactAndReturnData({
+        email,
+        phoneNumber,
+        linkedId: emailMatchPrimary.id,
+        linkPrecedence: "secondary",
+      });
+
+      return res.status(200).json(newContact);
+    } else if (!emailMatch && phoneMatch) {
+      // There is phone match who's primary will be the primary of new contact
+      const phoneMatchPrimary =
+        phoneMatch.linkPrecedence === "primary"
+          ? phoneMatch
+          : phoneMatch.linkedContact;
+      if (!phoneMatchPrimary) {
+        throw new Error("Sever Error");
+      }
+
+      const newContact = await createContactAndReturnData({
+        email,
+        phoneNumber,
+        linkedId: phoneMatchPrimary.id,
+        linkPrecedence: "secondary",
+      });
+
+      return res.status(200).json(newContact);
+    } else {
+      if (!emailMatch || !phoneMatch) {
+        throw new Error("Unreachable, but TS doesn't know that");
+      }
+
+      // Now we have run into case where both match is present
+      const phoneMatchPrimary =
+        phoneMatch.linkPrecedence === "primary"
+          ? phoneMatch
+          : phoneMatch.linkedContact;
+
+      if (!phoneMatchPrimary) {
+        throw new Error("Sever Error");
+      }
+
+      const emailMatchPrimary =
+        emailMatch.linkPrecedence === "primary"
+          ? emailMatch
+          : emailMatch.linkedContact;
+      if (!emailMatchPrimary) {
+        throw new Error("Sever Error");
+      }
+
+      if (phoneMatchPrimary.id === emailMatchPrimary.id) {
+        return res.status(200).json(prepareOutput(phoneMatch));
+      }
+
+      if (phoneMatchPrimary.createdAt >= emailMatchPrimary.createdAt) {
+        // email match primary have a higher rank and will stay
+        await prisma.contact.updateMany({
+          where: {
+            id: {
+              in: [
+                phoneMatchPrimary.id,
+                ...phoneMatchPrimary.linkingContacts.flatMap((lc) => lc.id),
+              ],
+            },
+          },
+          data: {
+            linkedId: emailMatchPrimary.id,
+            linkPrecedence: "secondary",
+          },
+        });
+      } else {
+        // phone match primary have a higher rank and will stay
+        await prisma.contact.updateMany({
+          where: {
+            id: {
+              in: [
+                emailMatchPrimary.id,
+                ...emailMatchPrimary.linkingContacts.flatMap((lc) => lc.id),
+              ],
+            },
+          },
+          data: {
+            linkedId: phoneMatchPrimary.id,
+            linkPrecedence: "secondary",
+          },
+        });
+      }
+
+      return res.status(200).json(prepareOutput(emailMatch));
+    }
   } catch (error) {
     if (error instanceof z.ZodError) {
       return res.status(400).json({
